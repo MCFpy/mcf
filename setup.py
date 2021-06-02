e6d79e104da4b33fcd8400b5c1b2da9e7f1dc875
hannahbus <38669089+hannahbus@users.noreply.github.com>
2021-06-02 16:31:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'mcf',
   packages = ['mcf'],
-  version = '0.0.0.2',
+  version = '0.0.0.3',
   license='MIT',
   description = 'mcf lets you estimate heterogeneous treatment effects for multiple treatment models in a selection-on-observables setting.',
   author = 'mlechner',                   
   author_email = 'michael.lechner@unisg.ch',      
   url = 'https://github.com/MCFpy/mcf',   
-  download_url = 'https://github.com/MCFpy/mcf/archive/refs/tags/v0.0.0.2.tar.gz',    
+  download_url = 'https://github.com/MCFpy/mcf/archive/refs/tags/v0.0.0.3.tar.gz',    
   keywords = ['causal machine learning, heterogeneous treatment effects, causal forests'],  
   classifiers=[
     'Development Status :: 4 - Beta',

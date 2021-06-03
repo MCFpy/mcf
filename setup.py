6438b80840b2fb91ea68bb26ff32853f76716ddf
hannahbus <38669089+hannahbus@users.noreply.github.com>
2021-06-03 20:53:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
   description = 'mcf is a powerful package to estimate heterogeneous treatment effects for multiple treatment models in a selection-on-observables setting',
   author = 'mlechner',                   
   author_email = 'michael.lechner@unisg.ch',      
-  url = 'https://github.com/MCFpy/mcf',   
-  download_url = 'https://github.com/MCFpy/mcf/archive/refs/tags/v1.1.0.tar.gz',    
+  url = 'https://github.com/MCFpy/mcf',      
   keywords = ['causal machine learning, heterogeneous treatment effects, causal forests'],  
   long_description=read('README'),
   classifiers=[

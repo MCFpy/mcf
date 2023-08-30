ea5f341556ee5466ddcd9be56f91a1626f6b353d
hannahbus <38669089+hannahbus@users.noreply.github.com>
2023-08-30 20:47:55 +0200

    Update all_parameters_mcf.py

--- examples/all_parameters_mcf.py
+++ examples/all_parameters_mcf.py
@@ -22,8 +22,7 @@ import pickle
 
 import pandas as pd
 
-from mcf.mcf_functions import ModifiedCausalForest
-# from mcf import ModifiedCausalForest
+from mcf import ModifiedCausalForest
 
 # ------------------ NOT parameters of the ModifiedCausalForest ---------------
 #  Define data to be used in this example

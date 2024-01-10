402b36ce2efbdb61daf0f94e376aadd141449c38
Joel Frischknecht <joda.fri@hotmail.com>
2024-01-10 09:43:59 +0100

    Clean up configuration script for Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
-#
+
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -9,13 +9,11 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath('../../mcf'))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -26,8 +24,7 @@ author = 'ML'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
@@ -41,31 +38,34 @@ extensions = [
     'sphinx.ext.doctest',
 ]
 
-napoleon_use_ivar = False
-
-# This will allow your docs to import the example code without requiring those modules be installed
-autodoc_mock_imports = ['bs4', 'requests']
-
-autoclass_content = 'class'
-autosummary_generate = True
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files. This pattern also
+# affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The suffix(es) of source filenames. You can specify multiple suffix as a list
+# of string:
 source_suffix = ['.rst', '.md']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# This will allow your docs to import the example code without requiring those
+# modules be installed
+autodoc_mock_imports = ['bs4', 'requests']
+
+# This ensures that the autoclass directive will only include the class'
+# docstring without the docstring of the __init__method.
+autoclass_content = 'class'
+
+# This will generate stub documentation pages for items included in autosummary
+# directives (even if those autosummary directives are "commented out"!).
+autosummary_generate = True
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
+# The theme to use for HTML and HTML Help pages.
 html_theme = 'pydata_sphinx_theme'
 
 # This removes the primary (left) sidebar from all pages of the documentation:
@@ -73,17 +73,10 @@ html_sidebars = {
   "**": []
 }
 
-# Custom sidebar templates, maps page names to templates.
-# html_sidebars = {
-#    "index": [
-#        "sidebar_versions.html",
-#        ]}
-
 html_theme_options = {
-    "show_nav_level": 2,
-    "navigation_depth": 4,
+    # Depth of the table of contents shown in the secondary (right) sidebar
     "show_toc_level": 5,
-    "show_version_warning_banner": True,
+    # Links in the navigation bar to GitHub and PyPI
     "icon_links": [
         {
             "name": "GitHub",

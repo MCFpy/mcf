1d0f8939000862ae86518291c3d14994cc0733a8
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2024-03-28 16:22:02 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../mcf'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'mcf 0.5.0'
+project = 'mcf 0.5.1'
 copyright = '2024, Michael Lechner'
 author = 'Michael Lechner'
 

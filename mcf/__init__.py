7cd865391e09086e341d1b14dab062fa6253775e
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2025-08-20 15:58:07 +0200

    Update __init__.py

--- mcf/__init__.py
+++ mcf/__init__.py
@@ -2,6 +2,3 @@ from mcf.mcf_main import ModifiedCausalForest
 from mcf.optpolicy_main import OptimalPolicy
 from mcf.reporting import McfOptPolReport
 from mcf.example_data_functions import example_data
-
-__all__ = ["ModifiedCausalForest", "OptimalPolicy", "McfOptPolReport", "example_data"]
-

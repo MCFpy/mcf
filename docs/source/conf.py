6d8aa44fe0758e67d683708d46e99bd3576de94f
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2025-08-20 13:57:17 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,9 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('..'))
 # sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../..'))
 # sys.path.insert(0, os.path.abspath('../../mcf/mcf'))
-sys.path.insert(0, os.path.abspath('../../mcf'))
+# sys.path.insert(0, os.path.abspath('../../mcf'))
 
 # -- Project information -----------------------------------------------------
 

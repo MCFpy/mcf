3f1ae11ab17d17d69c51c71c0e28e24f15e260dc
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2025-08-20 15:51:13 +0200

    Update __init__.py adding mcf

--- mcf/__init__.py
+++ mcf/__init__.py
@@ -1,7 +1,7 @@
-from .mcf_main import ModifiedCausalForest
-from .optpolicy_main import OptimalPolicy
-from .reporting import McfOptPolReport
-from .example_data_functions import example_data
+from mcf.mcf_main import ModifiedCausalForest
+from mcf.optpolicy_main import OptimalPolicy
+from mcf.reporting import McfOptPolReport
+from mcf.example_data_functions import example_data
 
 __all__ = ["ModifiedCausalForest", "OptimalPolicy", "McfOptPolReport", "example_data"]
 

8ae95b80314958ac31fd5e9c19aade68dcc3f245
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2025-12-08 09:58:36 +0100

    Update conf.py to 0.9.0 version of the mcf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../../mcf'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'mcf 0.8.0'
+project = 'mcf 0.9.0'
 copyright = '2024, Michael Lechner'
 author = 'Michael Lechner'
 
